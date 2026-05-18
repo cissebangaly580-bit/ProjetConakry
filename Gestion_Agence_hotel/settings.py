86752fb4cf839efd9391c9cf8483045beafefe67
cissebangaly580-bit <cissebangaly580@gmail.com>
2026-05-18 18:30:31 +0100

    Securisation : ajout du .gitignore et suppression des secrets

--- Gestion_Agence_hotel/settings.py
+++ Gestion_Agence_hotel/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -84,11 +88,11 @@ WSGI_APPLICATION = "Gestion_Agence_hotel.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'conakry_db',
-        'USER': 'postgres',
-        'PASSWORD': 'cisse2002',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HoST','localhost'),
+        'PORT': os.getenv('DB_PORT','5432'),
     }
 }
 
