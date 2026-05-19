dfc97538b53fd8faa1f18f110e12e1243b42cb56
cissebangaly580-bit <cissebangaly580@gmail.com>
2026-05-20 00:28:52 +0100

    encore des modification

--- Gestion_Agence_hotel/settings.py
+++ Gestion_Agence_hotel/settings.py
@@ -11,11 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-import os
-from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,17 +81,17 @@ WSGI_APPLICATION = "Gestion_Agence_hotel.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HoST','localhost'),
-        'PORT': os.getenv('DB_PORT','5432'),
+        'NAME': 'conakry_db',  # ← Vérifie qu'il n'y a PAS d'accent ici
+        'USER': 'postgres',
+        'PASSWORD': 'cisse2002',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -132,3 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # ← Pointe vers le dossier global à la racine
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"  # ← Dossier de collecte pour la production
